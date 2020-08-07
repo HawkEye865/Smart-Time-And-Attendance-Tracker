ed8173170f5ed8b4d6fa02b39f98cd7a2a5fd5d0
munasheghub <54100883+munasheghub@users.noreply.github.com>
2020-08-07 09:01:55 +0200

    Figured out modal content testing

--- STAT/src/app/dashboard/teams/teams.component.spec.ts
+++ STAT/src/app/dashboard/teams/teams.component.spec.ts
@@ -1,25 +1,57 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TeamsComponent } from './teams.component';
+import { HttpClientTestingModule,HttpTestingController  } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
-import { TeamsComponent } from './Teams.component';
+describe('Unit tests', () => {
+  describe('TeamsComponent', () => {
+    let component: TeamsComponent;
+    let fixture: ComponentFixture<TeamsComponent>;
+    let de: DebugElement;
+    let el: HTMLElement;
+    let autoModal : NgbModal;
+    let autoModalRef : NgbModalRef;
 
-describe('TeamsComponent', () => {
-  let component: TeamsComponent;
-  let fixture: ComponentFixture<TeamsComponent>;
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ TeamsComponent ]
+      })
+      .compileComponents();
+    }));
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TeamsComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(() => {
+      fixture = TestBed.createComponent(TeamsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TeamsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      de = fixture.debugElement.query(By.css('.row'));
+      el = de.nativeElement;
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    it('should be created', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should open the add team modal', async(() => {
+      //spyOn(component,'open');
+      el = fixture.debugElement.query(By.css("#add-team")).nativeElement;
+      //expect(el).toBeTruthy();
+      const contentBeforeClick = document.querySelector(".modal-content");
+      expect(contentBeforeClick).toBeFalsy();
+      el.click();
+      const contentAfterClick = document.querySelector(".modal-content");
+      expect(contentAfterClick).toBeTruthy();
+
+      document.querySelector(".modal-header h2").textContent = 'John';
+
+      //fixture.detectChanges();
+      //expect(component.open).toHaveBeenCalledTimes(1);
+    }));
   });
 });
