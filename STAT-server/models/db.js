1aca590aa94ebfd4403c1086d7d3edf04eea8f9f
munasheghub <54100883+munasheghub@users.noreply.github.com>
2020-09-11 17:48:28 +0200

    Automated test run - attempt #7

--- STAT-server/models/db.js
+++ STAT-server/models/db.js
@@ -21,7 +21,7 @@
 */ 
 
 const mongoose = require('mongoose');
-require("../config/config.js");
+require("../config/conf.js");
 
 mongoose.connect(process.env.MONGODB_URI, (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
